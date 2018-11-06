1329ff6734640031444b761824de200bed117a51
Jeremy Pollock <jpollock911@gmail.com>
2018-11-06 11:23:43 -0800

    fixing pagination and making it generic channel support

--- java/presence-search-sample-gradle/src/main/java/PresenceSearch.java
+++ java/presence-search-sample-gradle/src/main/java/PresenceSearch.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.pubnub.api.PNConfiguration;
@@ -14,18 +15,18 @@ public class PresenceSearch {
 	static PubNub pubnub;
 	static int limit = 100;
 	
-	private ArrayList<PNHereNowOccupantData> baseHereNowGetAllPages(boolean includeUuids, boolean includeState, Map<String, String> queryParams) {
+	private ArrayList<PNHereNowOccupantData> baseHereNowGetAllPages(ArrayList channels, boolean includeUuids, boolean includeState, Map<String, String> queryParams) {
 		ArrayList<PNHereNowOccupantData> occupants = new ArrayList<PNHereNowOccupantData>();
-
+		// this method will iterate over channels
 		
 		try {
 			int totalCount = -1;
 			int offset = 0;
-			
+						
 			while (totalCount == -1 || offset < totalCount) {
 				queryParams.put("offset", Integer.toString(offset));
 				PNHereNowResult response = pubnub.hereNow()
-						.channels(Arrays.asList("cgm_data"))
+						.channels(channels)
 						.includeUUIDs(includeUuids)
 						.includeState(includeState)
 						.queryParam(queryParams)
@@ -36,9 +37,8 @@ public class PresenceSearch {
 	                	occupants.add(occupant);
 		                offset++;	                	
 	                }	                
-	                totalCount = channelData.getOccupancy();
-
 	            }
+	            totalCount = response.getTotalOccupancy();
 			}
 			
 		} catch (PubNubException e) {
@@ -50,64 +50,70 @@ public class PresenceSearch {
 	}
 	
 	
-	public void hereNowShowUuidsNoStateSortDesc() {
+	public void hereNowShowUuidsNoStateSortDesc(ArrayList channels) {
         Map<String, String> queryParams = new HashMap<>();
         queryParams.put("limit", Integer.toString(limit));
         queryParams.put("sort", "value:desc");
 
-        ArrayList<PNHereNowOccupantData> occupants = baseHereNowGetAllPages(true, false, queryParams);
+        ArrayList<PNHereNowOccupantData> occupants = baseHereNowGetAllPages(channels, true, false, queryParams);
 		System.out.println(occupants.size());
 	}
-	public void hereNowShowUuidsShowStateSortDesc() {
+	public void hereNowShowUuidsShowStateSortDesc(ArrayList channels) {
         Map<String, String> queryParams = new HashMap<>();
         queryParams.put("limit", Integer.toString(limit));
         queryParams.put("sort", "value:desc");
 
-        ArrayList<PNHereNowOccupantData> occupants = baseHereNowGetAllPages(true, true, queryParams);
+        ArrayList<PNHereNowOccupantData> occupants = baseHereNowGetAllPages(channels, true, true, queryParams);
 		System.out.println(occupants.size());
 	}
 
-	public void hereNowShowUuidsShowStateSortDescSingleFilter() {
+	public void hereNowShowUuidsShowStateSortDescSingleFilter(ArrayList channels) {
         Map<String, String> queryParams = new HashMap<>();
         queryParams.put("limit", Integer.toString(limit));
         queryParams.put("sort", "value:desc");
         queryParams.put("filter", "value:gte:150");
 
-        ArrayList<PNHereNowOccupantData> occupants = baseHereNowGetAllPages(true, true, queryParams);
+        ArrayList<PNHereNowOccupantData> occupants = baseHereNowGetAllPages(channels, true, true, queryParams);
 		System.out.println(occupants.size());		
 	}
 	
-	public void hereNowShowUuidsShowStateSortDescDoubleFilter() {
+	public void hereNowShowUuidsShowStateSortDescDoubleFilter(ArrayList channels) {
         Map<String, String> queryParams = new HashMap<>();
         queryParams.put("limit", Integer.toString(limit));
         queryParams.put("sort", "value:desc");
         queryParams.put("filter", "value:gte:150,value:lte:160");
 
-        ArrayList<PNHereNowOccupantData> occupants = baseHereNowGetAllPages(true, true, queryParams);
+        ArrayList<PNHereNowOccupantData> occupants = baseHereNowGetAllPages(channels, true, true, queryParams);
 		System.out.println(occupants.size());		
 	}
 
 	public static void main(String[] args) {
-		if (!(args.length == 1 || args.length == 2)) {
-			System.out.println("Usage: gradle run --args '<sub key> <optional: origin>");
-			System.out.println("\t\tExample: gradle run --args 'myreallylongsubkeystring'");
-			System.out.println("\t\tExample: gradle run --args 'myreallylongsubkeystring mycustom.pubnub.com'");
+		if (args.length < 1 || args.length > 3) {
+			System.out.println("Usage: gradle run --args '<sub key> <channel (comma delimited for multiple)> <optional: origin>");
+			System.out.println("\t\tExample: gradle run --args 'myreallylongsubkeystring mychannel1,mychannel2'");
+			System.out.println("\t\tExample: gradle run --args 'myreallylongsubkeystring mychannel1,mychannel2 mycustom.pubnub.com'");
 			System.exit(0);
 		}
 		PNConfiguration pnConfiguration = new PNConfiguration();
 		pnConfiguration.setSecure(false);
 		pnConfiguration.setSubscribeKey(args[0]);
-		if (args.length == 2) {
-			pnConfiguration.setOrigin(args[1]);	
+		if (args.length == 3) {
+			pnConfiguration.setOrigin(args[2]);	
 		}
+		
+		String channels = args[1];
 		
 		pubnub = new PubNub(pnConfiguration);
 		
 		PresenceSearch app = new PresenceSearch();
-		app.hereNowShowUuidsNoStateSortDesc();
-		app.hereNowShowUuidsShowStateSortDesc();
-		app.hereNowShowUuidsShowStateSortDescSingleFilter();
-		app.hereNowShowUuidsShowStateSortDescDoubleFilter();
+		;
+
+		ArrayList channelsList= new ArrayList(Arrays.asList(channels.split(",")));
+		
+		app.hereNowShowUuidsNoStateSortDesc(channelsList);
+		app.hereNowShowUuidsShowStateSortDesc(channelsList);
+		app.hereNowShowUuidsShowStateSortDescSingleFilter(channelsList);
+		app.hereNowShowUuidsShowStateSortDescDoubleFilter(channelsList);
 		
 
 	}
